f6f45393802d12fddea4a563e741d89dcda58fe9
Parish350 <parishpugazh@gmail.com>
2025-06-11 18:10:36 +0100

    Update mydigitalclock2.py

--- mydigitalclock2.py
+++ mydigitalclock2.py
@@ -1,1 +1,14 @@
-
+from tkinter import*
+from time import strftime
+gui = Tk()
+gui.config(background="springgreen")
+gui.title("Digital Clock")
+gui.geometry("250x140")
+def time():
+    string = strftime('%I:%M:%S:%p:%B:%Y')
+    IbI.config(text=string)
+    IbI.after(1000,time)
+IbI = Label(gui, font=('calibri',40,'bold'),background="springgreen",foreground="orange")
+IbI.place (x = 100 ,y = 100)
+time()
+gui.mainloop()
