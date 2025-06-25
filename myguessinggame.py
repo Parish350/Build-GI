5e1c470a3b04671b6cf87ffd11a1f99636da4714
Parish350 <parishpugazh@gmail.com>
2025-06-25 17:23:28 +0100

    Update myguessinggame.py

--- myguessinggame.py
+++ myguessinggame.py
@@ -5,6 +5,19 @@ gui.title("GuessingGame")
 gui.geometry("250x140")
 
 import tkinter.messagebox
+import random  
+num = random.randint(1,20)
+def check_num():
+ n = number.get()
+ n = int(n)
+ if n > num:
+  tkinter.messagebox.showinfo("high","Your guess is high") 
+ elif n < num:  
+   tkinter.messagebox.showinfo("low","Your guess is low")  
+ elif n == num:
+  tkinter.messagebox.showinfo("correct","Your guess is correct")
+
+
 def getname():
  myName = name.get()
  tkinter.messagebox.showinfo("name", 'Well,' + myName + ',I am thinking of a number between 1 and 20.')
@@ -20,12 +33,13 @@ title3.place(x = 100 , y = 300)
 
 ok = Button (gui,text ="ok", width = 15 ,bg = "gray99", fg = "SpringGreen2" , command = getname)
 ok.place(x = 400, y = 200)
-guess = Button (gui,text ="guess", width = 15 ,bg = "gray99", fg = "SpringGreen2")
+guess = Button (gui,text ="guess", width = 15 ,bg = "gray99", fg = "SpringGreen2", command=check_num)
 guess.place(x = 550, y = 300)
 number = Entry(gui)
 number.place(x = 300, y = 330)
 
 exit = Button (gui,text ="exit", width = 15 ,bg = "gray99", fg = "SpringGreen2" , command = exit)
 exit.place(x = 400, y = 250)
+
 
 gui.mainloop()
