c8a600de720264022344a616db90b60b49187c3a
Parish350 <parishpugazh@gmail.com>
2025-07-09 18:00:38 +0100

    Update mycombobox.py

--- mycombobox.py
+++ mycombobox.py
@@ -4,33 +4,50 @@ root = Tk()
 root.config(background="gray25")
 root.title("Lists")
 root.geometry("250x150")
-def submit_list():
-    choice = theOption1.get()
-    tkinter.messagebox.showinfo("lists","lists sucessfully correct") 
+def add_list():
+ item = listinput.get()
+ listbox.insert(END,item)
+ listinput.delete(0,END)
 from tkinter.ttk import*
+from tkinter.filedialog import*
 
 
 
-title1 = Label (root, text = "Save") 
-title1.place(x = 90 , y = 50)
-title2 = Label (root, text = "Delete") 
-title2.place(x = 90 , y = 100)
-title3 = Label (root, text = "Add") 
-title3.place(x = 90 , y = 50)
-title4 = Label (root, text = "Open") 
-title4.place(x = 90 , y = 100)
-theOption1 = StringVar()
-options1 = Combobox(root,textvariable = theOption1, width = 20, value = theOption1,fg = "red" ,bg="gray99" )
-options1['values'] = ("List1","List2","List3","List4","List5","List6","List7","List8","List9","List10")
-options1.place(x = 300 ,y = 180)
-theOption2 = IntVar()
-options2 = Combobox(root,textvariable = theOption2, width = 20, value = theOption2,fg = "red" ,bg="gray99" )
-options2['values'] = tuple(range(11))
-options2.place(x = 300 ,y = 210)
-combo = Entry(root)
-combo.place(x = 210, y = 300)
-order = Button (root,text ="order", command = submit_list)
-order.place(x = 330, y = 250)
-list = Label (root,text ="Select your list")
-list.place(x = 100, y = 180)
+
+def delete_list():
+ item = listbox.curselection()
+ if item:
+  listbox.delete(item)
+
+def saveFile():
+ file=asksaveasfile(defaultextension = ".text")
+ if file is not None:
+  for item in listbox.get(0,END):
+   print(item.strip(),file=file)
+ listbox.delete(0,END)
+
+
+title1 = Button (root, text = "Save", command = saveFile) 
+title1.place(x = 100 , y = 100)
+title2 = Button (root, text = "Delete", command = delete_list) 
+title2.place(x = 600 , y = 100)
+title3 = Button (root, text = "Add", command = add_list)
+title3.place(x = 100 , y = 200)
+title4 = Button (root, text = "Open") 
+title4.place(x = 600 , y = 200)
+listinput = Entry()
+listinput.place(x = 300 , y = 150)
+frame = Frame(root)
+scrollbar = Scrollbar(frame, orient = "vertical")
+scrollbar.pack(side = RIGHT, fill = Y)
+listbox = Listbox(frame,width = 70, yscrollcommand=scrollbar.set, bg = "red")
+listbox.pack(side = LEFT, padx = 5)
+scrollbar.config(command = listbox.yview)
+frame.place(x = 150, y = 250)
+
+
+
+
+
+
 root.mainloop()
