672c175df5c788d343abf17b47cb1e96c85bba7a
Parish350 <parishpugazh@gmail.com>
2025-07-30 18:00:33 +0530

    Create myspeedtranslator.py

---
+++ myspeedtranslator.py
@@ -0,0 +1,17 @@
+from gtts import gTTS
+from translate import Translator
+import os
+from tkinter import*
+gui = Tk()
+gui.config(background="medium spring green")
+gui.title("Text to Speech Converter")
+selection=StringVar()
+world={'Spanish':'es','French':'fr','Tamil':'ta'}
+r1=Radiobutton(gui,text='Spanish',variable=selection,value='es')
+r1.place(x = 100 ,y = 100)
+r2=Radiobutton(gui,text='French',variable=selection,value='fr')
+r2.place(x = 200 ,y = 100)
+r3=Radiobutton(gui,text='Tamil',variable=selection,value='ta')
+r3.place(x = 300 ,y = 100)
+gui.geometry("250x140")
+gui.mainloop()
