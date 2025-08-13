6ae85a61c170a3f518ff76a930c157d4387fa1ba
Parish350 <parishpugazh@gmail.com>
2025-08-13 17:55:15 +0530

    Update myjumbledwordgame.py

--- myjumbledwordgame.py
+++ myjumbledwordgame.py
@@ -5,15 +5,31 @@ gui = Tk()
 gui.config(background="dark green")
 gui.title("JUMBLED WORD GAME")
 selection=StringVar()
-title1 = Label (gui, text = "elov",fg = "mint cream" ,bg="dark green", font = ( "times",25,"bold")) 
+words = ['Banana','Orange','Strawberries']
+jumbled = ['nabnaa','engroa','bwerstraresi']
+Count = 0
+def reset():
+    global words,Count,jumbled
+    title1.config(text = jumbled[Count])
+def Check():
+    global words,Count,jumbled
+    user = Label.get()
+    if user == words[Count]:
+     tkinter.messagebox.showinfo("Congratlations","You got the right answer.") 
+    else:
+     tkinter.messagebox.showinfo("Sorry","You got the wrong answer,try again.")   
+    Count += 1
+    reset()
+
+title1 = Label (gui, text = "",fg = "mint cream" ,bg="dark green", font = ( "times",25,"bold")) 
 title1.place(x = 90 , y = 50)
-title2 = Label (gui, text = "Check",fg = "green2" ,bg="dark green", font = ( "times",28,"bold")) 
-title2.place(x = 100 , y = 100)
-title3 = Label (gui, text = "Reset",fg = "yellow" ,bg="dark green", font = ( "times",28,"bold")) 
-title3.place(x = 100 , y = 200)
+title2 = Button (gui, text = "Check",fg = "green2" ,bg="dark green", font = ( "times",28,"bold"),command = Check) 
+title2.place(x = 100 , y = 150)
+title3 = Button (gui, text = "Reset",fg = "yellow" ,bg="dark green", font = ( "times",28,"bold"), command = reset) 
+title3.place(x = 100 , y = 250)
 Label = Entry()
-Label.place(x = 100 , y = 150)
+Label.place(x = 100 , y = 100)
 
-
+reset()
 gui.geometry("250x140")
 gui.mainloop()
